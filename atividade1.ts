09585e913314f39e77c4f82f0463caed59e7299e
devluis1209 <luispaulodaconceicaopereira@gmail.com>
2025-11-21 10:14:43 -0300

    primeira alteração

---
+++ atividade1.ts
@@ -0,0 +1,7 @@
+// 1. Declare três variáveis com tipos explícitos
+let nome: string = "João";
+let idade: number = 20;
+let ativo: boolean = true;
+
+// Exiba no console uma frase
+console.log(`Usuário ${nome}, idade ${idade}, está ativo? ${ativo}`);
