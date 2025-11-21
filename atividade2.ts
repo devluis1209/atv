db4226516ffcb11b439553ae5e60e42555d2e1b3
devluis1209 <luispaulodaconceicaopereira@gmail.com>
2025-11-21 10:19:55 -0300

    primeira alteração atv2

---
+++ atividade2.ts
@@ -0,0 +1,9 @@
+// 2. Crie uma variável nota que pode ser number ou string
+let nota: number | string;
+
+// Atribua valores diferentes e exiba o tipo atual
+nota = 9.5;
+console.log(`Valor: ${nota}, Tipo: ${typeof nota}`);
+
+nota = "A+";
+console.log(`Valor: ${nota}, Tipo: ${typeof nota}`);
