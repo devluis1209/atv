8c74502a22532a376ac4cb57d0ff0f61b07ad471
devluis1209 <luispaulodaconceicaopereira@gmail.com>
2025-11-21 10:22:42 -0300

    primeira alteração atv 3

---
+++ atividade3.ts
@@ -0,0 +1,8 @@
+// 3. Crie uma função somar
+function somar(a: number, b: number): number {
+    return a + b;
+}
+
+// Exiba no console o resultado usando template string
+const resultado: number = somar(5, 3);
+console.log(`A soma é ${resultado}`);
